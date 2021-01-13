8e32ce175e079429e0cb397b0af0fd88fff30ffe
mailatalamisan <76142191+mailatalamisan@users.noreply.github.com>
2021-01-13 11:37:54 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const list = document.querySelector('#book-list ul');
 
-list.addEventListener('click' function(e){
-    if(e.target.className == 'delete'){
-        const li = e.target.parentElement;
-        list.removeChild(li);
-    }
-})
+// delete books
+list.addEventListener('click', function(e){
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    list.removeChild(li);
+  }
+});
+
+
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+  console.log(value);
+});
